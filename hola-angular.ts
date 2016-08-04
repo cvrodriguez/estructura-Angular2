a5d587d4af5cf5ed96325a0d8abee9e859f25ab7
cvrodriguez <cinvir15@gmail.com>
2016-08-03 20:57:39 -0500

    first commit

---
+++ hola-angular.ts
@@ -0,0 +1,23 @@
+	import {Component} from '@angular/core';
+	import {bootstrap} from '@angular/platform-browser-dynamic';
+
+	@Component({
+	selector: 'hola-angular',
+	template: "<h1>'{{saludo}} </h1>"
+})
+
+	class HolaAngularComponent{
+	saludo: string;
+	constructor(){
+	this.saludo = '¡Hola Pablo ;* !';
+
+   }
+}
+
+	bootstrap(HolaAngularComponent);
+ 
+
+ 
+
+
+
